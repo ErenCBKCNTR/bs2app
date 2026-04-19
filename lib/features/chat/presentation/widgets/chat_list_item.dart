11d47ba411cde9cb46502252b1a15aa07b7fabb6
ErenCBKCNTR <erencs87@gmail.com>
2026-04-19 12:34:36 +0300

    refactor: Update PocketbaseService import path
    The import path for `PocketbaseService` was incorrect. This commit corrects the path to `package:blind_social/core/services/pocketbase_service.dart`.

--- lib/features/chat/presentation/widgets/chat_list_item.dart
+++ lib/features/chat/presentation/widgets/chat_list_item.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
-import '../../../core/services/pocketbase_service.dart';
+import 'package:blind_social/core/services/pocketbase_service.dart';
 
 class ChatListItem extends StatelessWidget {
   final RecordModel chat;
